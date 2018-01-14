a360c221edc83ae0f24d32cb9fe25febe88b2d8e
zhangyue <zhangyue@beisen.com>
2018-01-15 00:04:57 +0800

    add Add Credentials

---
+++ jenkins/mounts/add-credential-with-ssh-key.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# 添加Jenkins服务器上生成的私钥，和Gitlab添加的.pub对应的Key
+
+HOST=http://127.0.0.1:8080
+ACCOUNT=root:12345678
+CRUMB=Jenkins-Crumb:1df73a8c77fdfdf0e4b67ff7e6cd095c
+# grep [^-]$: 获取行结尾不是-的内容，抓取私钥内容
+# tr -d '\n': 删除换行符号
+SSHKEY_PRIVATE_KEY=`cat /var/lib/jenkins/.ssh/id_rsa | grep [^-]$ | tr -d '\n'`
+
+# Jenkins > Credentials > (global) > Add Credentials
+curl -X POST -u "$ACCOUNT" -H "$CRUMB" $HOST/credentials/store/system/domain/_/createCredentials --data-urlencode 'json={
+  "": "0",
+  "credentials": {
+    "scope": "GLOBAL",
+    "id": "gitlab_root_user",
+    "username": "root",
+    "password": "",
+    "privateKeySource": {
+      "stapler-class": "com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey$DirectEntryPrivateKeySource",
+      "privateKey": "'$SSHKEY_PRIVATE_KEY'",
+    },
+    "description": "",
+    "stapler-class": "com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey"
+  }
+}'
--- jenkins/mounts/install-jenkins-plugins.sh
+++ jenkins/mounts/install-jenkins-plugins.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # >https://gist.github.com/basmussen/8182784
+# >https://support.cloudbees.com/hc/en-us/articles/219257077-CSRF-Protection-Explained
 # 命令的方式导入jenkins所需要的插件
 
 HOST=http://127.0.0.1:8080
-URL=/pluginManager/installNecessaryPlugins
 ACCOUNT=root:12345678
 CRUMB=Jenkins-Crumb:1df73a8c77fdfdf0e4b67ff7e6cd095c
 
 # 安装gitlab插件，和Gitlab对接需要在Jenkins安装此插件
-curl -XPOST -u "$ACCOUNT" -H "$CRUMB" $HOST$URL -d '<install plugin="gitlab-plugin@current"/>'
+curl -XPOST -u "$ACCOUNT" -H "$CRUMB" $HOST/pluginManager/installNecessaryPlugins -d '<install plugin="gitlab-plugin@current"/>'
 
 # wait 20 sec
 sleep 20
