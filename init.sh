a7e24354f56e206cb1ceb15b5e5e65b0939f63cb
zhangyue <zhangyue@beisen.com>
2018-01-16 02:25:48 +0800

    ini

--- init.sh
+++ init.sh
@@ -1,4 +1,7 @@
 
+# 进入jenkins服务器，添加credential
+docker exec jenkins /var/external-mounts/add-credential-with-ssh-key.sh
+
 # Gitlab配置完Root用户密码后，提取Private Token到mounts/gitlab-private-token中，再执行init.sh
 # 把jenkins服务器上的公钥push到Gitlab服务器上
-docker exec jenkins $external_mounts_path/add-ssh-key-to-gitlab.sh
+docker exec jenkins /var/external-mounts/add-ssh-key-to-gitlab.sh
