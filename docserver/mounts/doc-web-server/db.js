f096ab53664740c094d754ca06ebd2b2dd5eb70d
zhangyue <zhangyue@beisen.com>
2018-01-16 14:55:25 +0800

    doc server

---
+++ docserver/mounts/doc-web-server/db.js
@@ -0,0 +1,42 @@
+
+var MongoClient = require('mongodb').MongoClient
+
+var state = {
+  db: null
+}
+
+MongoClient.connect('mongodb://localhost:27017/test', function(err, db) {
+  if (err) return
+
+  var collection = db.collection('foods')
+  collection.insert({name: 'taco', tasty: true}, function(err, result) {
+    collection.find({name: 'taco'}).toArray(function(err, docs) {
+      console.log(docs[0])
+      db.close()
+    })
+  })
+})
+
+exports.connect = function(url, done) {
+  if (state.db) return done()
+
+  MongoClient.connect(url, function(err, db) {
+    if (err) return done(err)
+    state.db = db
+    done()
+  })
+}
+
+exports.get = function() {
+  return state.db
+}
+
+exports.close = function(done) {
+  if (state.db) {
+    state.db.close(function(err, result) {
+      state.db = null
+      state.mode = null
+      done(err)
+    })
+  }
+}
--- docserver/mounts/doc-web-server/routes/index.js
+++ docserver/mounts/doc-web-server/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.put('/api/add', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
 module.exports = router;
