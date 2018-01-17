bed149c27f76c5025f484117754c2dec1fb0f1d3
zhangyue <zhangyue@beisen.com>
2018-01-17 17:13:25 +0800

    doc

---
+++ mounts/spb-builder/extract-doc.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo workspace: $WORKSPACE
+ls -l
+
+# 取出来.builder.json中的有关doc的描述提交到文档站点
+curl "http://doc-server:3000/add?type=nav&aurthor=xuhaibin@beisne.com&keywords=aa,dd,gg,zhangyue"
