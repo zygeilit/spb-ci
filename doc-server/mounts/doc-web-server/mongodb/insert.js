46c96f1adcc1277911f630e0c6e0394e6b0fb982
zhangyue <zhangyue@beisen.com>
2018-01-17 15:11:48 +0800

    rename

--- doc-server/mounts/doc-web-server/mongodb/insert.js
+++
@@ -1,9 +0,0 @@
-var assert = require('assert')
-
-exports = function (colname, doc, done) {
-  var collection = this.db.collection(colname)
-  collection.insert(doc, function(err, result) {
-    assert.equal(null, err)
-    done(result)
-  })
-}
