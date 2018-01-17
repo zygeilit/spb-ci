e11d43b28b7ee15d2df95d872a65b177bad18aaf
zhangyue <zhangyue@beisen.com>
2018-01-17 14:25:34 +0800

    doc server node version

--- doc-server/mounts/doc-web-server/db.js
+++ doc-server/mounts/doc-web-server/db.js
@@ -1,42 +1,52 @@
-var MongoClient = require('mongodb').MongoClient
+// var MongoClient = require('mongodb').MongoClient
 
 var state = {
   db: null
 }
 
-MongoClient.connect('mongodb://mongodb:27017/test', function(err, db) {
-  if (err) return
-  var collection = db.collection('foods')
-  collection.insert({name: 'taco', tasty: true}, function(err, result) {
-    collection.find({name: 'taco'}).toArray(function(err, docs) {
-      console.log(docs[0])
-      db.close()
-    })
-  })
+// Retrieve
+var MongoClient = require('mongodb').MongoClient
+
+// Connect to the db
+MongoClient.connect("mongodb://localhost:27017/doc-web-server", function(err, db) {
+  if(!err) {
+    console.log("We are connected");
+  }
 })
 
-exports.state = state
+// MongoClient.connect('mongodb://mongodb:27017/test', function(err, db) {
+//   if (err) return
+//   var collection = db.collection('foods')
+//   collection.insert({name: 'taco', tasty: true}, function(err, result) {
+//     collection.find({name: 'taco'}).toArray(function(err, docs) {
+//       console.log(docs[0])
+//       db.close()
+//     })
+//   })
+// })
 
-exports.connect = function(url, done) {
-  if (state.db) return done()
+// exports.state = state
 
-  MongoClient.connect(url, function(err, db) {
-    if (err) return done(err)
-    state.db = db
-    done()
-  })
-}
+// exports.connect = function(url, done) {
+//   if (state.db) return done()
 
-exports.get = function() {
-  return state.db
-}
+//   MongoClient.connect(url, function(err, db) {
+//     if (err) return done(err)
+//     state.db = db
+//     done()
+//   })
+// }
 
-exports.close = function(done) {
-  if (state.db) {
-    state.db.close(function(err, result) {
-      state.db = null
-      state.mode = null
-      done(err)
-    })
-  }
-}
+// exports.get = function() {
+//   return state.db
+// }
+
+// exports.close = function(done) {
+//   if (state.db) {
+//     state.db.close(function(err, result) {
+//       state.db = null
+//       state.mode = null
+//       done(err)
+//     })
+//   }
+// }
