4e1447e58c887b722dba26883d3ad5de79a1becc
zhangyue <zhangyue@beisen.com>
2018-01-19 12:58:56 +0800

    register

--- mounts/gitlab-ci/gitlab-runner-register.sh
+++ mounts/gitlab-ci/gitlab-runner-register.sh
@@ -2,11 +2,14 @@
 # link:
 # 1. # 2. https://blog.m31271n.com/2017/08/22/%E9%80%9A%E8%BF%87-GitLab-%E6%9E%84%E5%BB%BA%E8%87%AA%E5%B7%B1%E7%9A%84-CI-CD-Pipeline/
 
+REG_TOKEN=$1
+NAME=$2
+
 docker exec -it gitlab-runner gitlab-runner register \
   --non-interactive \
-  --url https://gitlab.example.com/ \
-  --registration-token ${REG_TOKEN} \
-  --name ${NAME} \
+  --url http://$gitlab_host_name/ \
+  --registration-token $1 \
+  --name $2 \
   --tag-list docker-in-docker \
   # false
   --run-untagged=true \
