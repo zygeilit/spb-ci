5e51c5271834184a10105bca28f74f9caab2bbe6
zhangyue <zhangyue@beisen.com>
2018-01-19 14:35:44 +0800

    gitlab-ci

--- mounts/gitlab-ci/gitlab-runner-register.sh
+++ mounts/gitlab-ci/gitlab-runner-register.sh
@@ -3,8 +3,8 @@
 # 1. https://blog.m31271n.com/2017/08/22/%E9%80%9A%E8%BF%87-GitLab-%E6%9E%84%E5%BB%BA%E8%87%AA%E5%B7%B1%E7%9A%84-CI-CD-Pipeline/
 # 2. https://docs.gitlab.com/ce/ci/docker/using_docker_build.html
 
-REG_TOKEN=$1
-NAME=$2
+REG_TOKEN=MFxQG6tyJx5W9zZNPGQz
+NAME=buttontest
 
 docker exec -it gitlab-runner gitlab-runner register \
   --non-interactive \
