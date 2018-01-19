6ea28439338440ed216dd2d72fb2f8416b6201aa
zhangyue <zhangyue@beisen.com>
2018-01-19 14:12:28 +0800

    gitlab-ci

--- mounts/gitlab-ci/gitlab-runner-register.sh
+++ mounts/gitlab-ci/gitlab-runner-register.sh
@@ -1,6 +1,7 @@
 # 非交互式注册方式
 # link:
-# 1. # 2. https://blog.m31271n.com/2017/08/22/%E9%80%9A%E8%BF%87-GitLab-%E6%9E%84%E5%BB%BA%E8%87%AA%E5%B7%B1%E7%9A%84-CI-CD-Pipeline/
+# 1. https://blog.m31271n.com/2017/08/22/%E9%80%9A%E8%BF%87-GitLab-%E6%9E%84%E5%BB%BA%E8%87%AA%E5%B7%B1%E7%9A%84-CI-CD-Pipeline/
+# 2. https://docs.gitlab.com/ce/ci/docker/using_docker_build.html
 
 REG_TOKEN=$1
 NAME=$2
