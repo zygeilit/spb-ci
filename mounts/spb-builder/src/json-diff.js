3805928c4684c5c58418d96ad62bc9107e4e5846
zhangyue <zhangyue@beisen.com>
2018-01-22 11:28:21 +0800

    json diff

--- mounts/spb-builder/src/json-diff.js
+++ mounts/spb-builder/src/json-diff.js
@@ -1,17 +1,23 @@
 require('colors')
 var jsdiff = require('diff')
 
-var one = 'beep boop';
-var other = 'beep boob blah';
+var one = {
+  a: 123,
+  b: 321
+};
+var other = {
+  a:123,
+  b: 4321
+};
 
-var diff = jsdiff.diffChars(one, other);
+var diff = jsdiff.diffJson(one, other);
 
 diff.forEach(function(part){
   // green for additions, red for deletions
   // grey for common parts
   var color = part.added ? 'green' :
-    part.removed ? 'red' : 'grey';
+    part.removed ? 'red' : 'grey'
   process.stderr.write(part.value[color]);
 });
 
-console.log();
+console.log()
