a52e1a4acd3b0643570e3dd837d10f00328632ce
zhangyue <zhangyue@beisen.com>
2018-01-23 11:55:58 +0800

    ast new

--- mounts/spb-builder/src/ast-generate/index.js
+++ mounts/spb-builder/src/ast-generate/index.js
@@ -5,23 +5,29 @@ var fs = require('fs')
 require('acorn-jsx/inject')(acorn)
 require('acorn-es7-plugin')(acorn)
 
-var jsDomeFileContent = fs.readFileSync(`${__dirname}/demo-react-cmp.js`, { encoding: 'utf8' })
+var jsDomeFileContent = fs.readFileSync(`${__dirname}/react-todos/js/todoItem.js`, { encoding: 'utf8' })
 
 var ast = acorn.parse(jsDomeFileContent, {
   'sourceType': 'module', // 支持import/export
-  'locations': true, // 是否携带行号，列好
-  'plugins': { jsx: true },
-  'onComment': function (block, text, start, end, pos, pos1) {
-  	console.log('------------start----------------')
-  	console.log(block)
-  	console.log(text.trim())
-  	console.log(start)
-  	console.log(end)
-  	console.log(pos)
-  	console.log(pos1)
-  	console.log('-------------end------------')
-  }
+  'locations': false, // 是否携带行号，列好
+  // 'ranges': false,
+  'plugins': { jsx: true }
 })
 
-fs.writeFile(`${__dirname}/ast-demo-react-cmp.json`, JSON.stringify(ast, null, 2))
-console.log('Ast-Json Generated')
+// fs.writeFile(`${__dirname}/ast-react-cmp.json`, JSON.stringify(ast, null, 2))
+
+walk.findNodeAt(ast, null, null,
+  function (nodeType, node) {
+    // find: let|const|var <cmp-name> = React.createClass({ ... })
+    if (nodeType === 'CallExpression') {
+      let { type, object, property } = node.callee
+      // React.createClass
+      if (type === 'MemberExpression' && object.name === 'React' && property.name === 'createClass') {
+        let [{ properties }] = node.arguments
+        for (var i = 0; i < properties.length; i++) {
+          console.log(properties[i].key.name)
+        }
+      }
+    }
+  }
+)
